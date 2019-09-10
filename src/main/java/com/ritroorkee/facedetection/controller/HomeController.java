992b3806549efaa56a2a320aeedd715963d91e32
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-10 14:39:20 +0530

    register object is in place

--- src/main/java/com/ritroorkee/facedetection/controller/HomeController.java
+++ src/main/java/com/ritroorkee/facedetection/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.ritroorkee.facedetection.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ritroorkee.facedetection.model.User;
 
 @Controller
 public class HomeController {
@@ -17,7 +22,15 @@ public class HomeController {
 	}
 	
 	@GetMapping("/register")
-	public String register() {
+	public String register(Model model) {
+		model.addAttribute("user", new User());
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String register(@ModelAttribute("user")
+					User user, Model model) {
+		model.addAttribute("user", user);
 		return "register";
 	}
 }
