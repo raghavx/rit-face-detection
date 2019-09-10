47e37df32d377d80d0b1de2f6c16b4b3a518273d
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-10 12:44:46 +0530

    register page in place

---
+++ src/main/java/com/ritroorkee/facedetection/model/User.java
@@ -0,0 +1,36 @@
+package com.ritroorkee.facedetection.model;
+
+public class User {
+
+	private String userName;
+	
+	private String password;
+	
+	private String confirmPassword;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	
+}
