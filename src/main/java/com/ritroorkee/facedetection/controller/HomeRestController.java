57c66dcc8103bb2f56ffb9e64db9c5c1001ee074
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-11 12:25:37 +0530

    rest controller intro

--- src/main/java/com/ritroorkee/facedetection/controller/HomeRestController.java
+++ src/main/java/com/ritroorkee/facedetection/controller/HomeRestController.java
@@ -1,6 +1,8 @@
 package com.ritroorkee.facedetection.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ritroorkee.facedetection.model.User;
@@ -11,5 +13,11 @@ public class HomeRestController {
 	@GetMapping("/api/v1/user")
 	public User getUser() {
 		return new User();
+	}
+	
+	@PostMapping("/api/v1/user")
+	public User getUser( @RequestBody User user) {
+		System.out.println(user.getUserName());
+		return user;
 	}
 }
