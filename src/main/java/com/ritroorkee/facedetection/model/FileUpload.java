df261ff272ef9b4927e35b9443b45ae896754137
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-11 15:16:18 +0530

    assignment 11 sept

---
+++ src/main/java/com/ritroorkee/facedetection/model/FileUpload.java
@@ -0,0 +1,16 @@
+package com.ritroorkee.facedetection.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUpload {
+
+	private MultipartFile file;
+	
+	public MultipartFile getFile() {
+		return file;
+	}
+	
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+}
