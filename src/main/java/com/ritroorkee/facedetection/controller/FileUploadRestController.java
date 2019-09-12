ddc005c73b51d0e76eb477e7491d6946a2c6de4d
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-12 12:52:37 +0530

    upload three files

--- src/main/java/com/ritroorkee/facedetection/controller/FileUploadRestController.java
+++ src/main/java/com/ritroorkee/facedetection/controller/FileUploadRestController.java
@@ -1,11 +1,11 @@
 package com.ritroorkee.facedetection.controller;
 
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ritroorkee.facedetection.model.FileUpload;
 
@@ -19,9 +19,21 @@ public class FileUploadRestController {
 	}
 
 	@PostMapping("/api/v1/upload")
-	public FileUpload upload(@RequestBody FileUpload fileUpload) {
-		System.out.println(fileUpload.getFile().getName());
-
-		return fileUpload;
+	public ResponseEntity<String> upload(
+			@RequestParam("file") 
+			MultipartFile multipartFile,
+			@RequestParam("file1") 
+			MultipartFile multipartFile1,
+			@RequestParam("file2") 
+			MultipartFile multipartFile2) {
+		System.out.println(multipartFile.getOriginalFilename());
+		System.out.println(multipartFile1.getOriginalFilename());
+		
+		return ResponseEntity.ok(
+				multipartFile.getOriginalFilename()+" "+
+				multipartFile1.getOriginalFilename()+" "+
+				multipartFile2.getOriginalFilename());
 	}
+	
+	
 }
