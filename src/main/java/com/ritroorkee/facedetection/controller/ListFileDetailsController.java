0d1afbae8c4ba9ccbc0046133aa19309b655ff87
Raghvendra Sahu <rx.sahu@gmail.com>
2019-09-13 10:16:53 +0530

    file listing

--- src/main/java/com/ritroorkee/facedetection/controller/ListFileDetailsController.java
+++ src/main/java/com/ritroorkee/facedetection/controller/ListFileDetailsController.java
@@ -1,16 +1,29 @@
 package com.ritroorkee.facedetection.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ritroorkee.facedetection.model.FileDetails;
+import com.ritroorkee.facedetection.repository.FileDetailsRepository;
+
 @Controller
 public class ListFileDetailsController {
 
-	// TODO 
-	
+	@Autowired
+	private FileDetailsRepository fileDetailsRepository;
+
 	@GetMapping("/list/files")
 	public String listFiles(Model model) {
+		List<FileDetails> allFiles = fileDetailsRepository.findAll();
+		model.addAttribute("allFiles", allFiles);
+		
+		for (FileDetails fd : allFiles) {
+			System.out.println(fd.getName() + " " + fd.getPath());
+		}
 		return "listfiles";
 	}
 }
